e25ecb396833e72a3a66047fdfc22544110dc38a
Mohib Khan <mohibul.khan@mbp.local>
2022-01-21 17:18:18 +0530

    react-native with tailwind-rn

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { TailwindProvider, useTailwind } from "tailwind-rn";
+import utilities from "./tailwind.json";
 
 export default function App() {
+  const tailwind = useTailwind();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider utilities={utilities}>
+      <View>
+        <Text style={tailwind("text-blue-600")}>Hello world</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
